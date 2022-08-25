e7f3c633f8d50c7e78aa17d42324b9a8671c902b
kanno-y <kanno0521@gmail.com>
2022-08-26 01:35:54 +0900

    add: eslint, storybook追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
-import '../styles/globals.css';
-import type { AppProps } from 'next/app';
-import Head from 'next/head';
+import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import { createGlobalStyle } from 'styled-components'
 
 // グローバルのスタイル
 const GlobalStyle = createGlobalStyle`
@@ -26,24 +26,24 @@ const GlobalStyle = createGlobalStyle`
   ol, ul {
     list-style: none;
   }
-`;
+`
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <>
       <Head>
-        <meta key='charset' name='charset' content='utf-8' />
+        <meta key="charset" name="charset" content="utf-8" />
         <meta
-          key='viewport'
-          name='viewport'
-          content='width=device-width, initial-scale=1, shrink-to-fit=no, maximum-scale=5'
+          key="viewport"
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no, maximum-scale=5"
         />
-        <meta property='og:locale' content='ja_JP' />
-        <meta property='og:type' content='website' />
+        <meta property="og:locale" content="ja_JP" />
+        <meta property="og:type" content="website" />
         <GlobalStyle />
         <Component {...pageProps} />
       </Head>
     </>
-  );
-};
+  )
+}
 
-export default MyApp;
+export default MyApp
