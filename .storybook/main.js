0869358ba1b4fb86a09676e6e440c51e2e3fb1a4
kanno-y <kanno0521@gmail.com>
2022-09-19 15:52:30 +0900

    setup: main.jsの設定

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,32 +1,28 @@
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
+const path = require('path')
+
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-webpack5"
-  }
-  staticDirs: ["public"],
-  babel: async options => ({
+  staticDirs: ['public'],
+  babel: async (options) => ({
     ...options,
     plugins: [
-      "@babel/plugin-proposal-class-properties",
-      "@babel/plugin-proposal-private-methods",
-      "@babel/plugin-proposal-private-property-in-object",
+      '@babel/plugin-proposal-class-properties',
+      '@babel/plugin-proposal-private-methods',
+      '@babel/plugin-proposal-private-property-in-object',
     ],
   }),
   webpackFinal: async (confing) => {
     config.resolve.plugins = [
       new TsconfigPathsPlugin({
-        configFile: path.resolve(__dirname, "../tsconfig.json")
+        configFile: path.resolve(__dirname, '../tsconfig.json'),
       }),
-    ];
+    ]
     return config
-  }
+  },
 }
