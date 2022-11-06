d8650e6ff1ed9d8286e9c480aa325e98bfc54572
kanno-y <kanno0521@gmail.com>
2022-11-07 00:03:22 +0900

    fix: error修正

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -2,13 +2,13 @@ import pino from 'pino'
 import { createPinoBrowserSend, createWriteStream } from 'pino-logflare'
 
 const stream = createWriteStream({
-  apiKey: process.env.NEXT_PUBLIC_LOGFLARE_API_KEY,
-  sourceToken: process.env.NEXT_PUBLIC_LOGFLARE_SOURCE_ID,
+  apiKey: process.env.NEXT_PUBLIC_LOGFLARE_API_KEY ?? '',
+  sourceToken: process.env.NEXT_PUBLIC_LOGFLARE_SOURCE_ID ?? '',
 })
 
 const send = createPinoBrowserSend({
-  apiKey: process.env.NEXT_PUBLIC_LOGFLARE_API_KEY,
-  sourceToken: process.env.NEXT_PUBLIC_LOGFLARE_SOURCE_ID,
+  apiKey: process.env.NEXT_PUBLIC_LOGFLARE_API_KEY ?? '',
+  sourceToken: process.env.NEXT_PUBLIC_LOGFLARE_SOURCE_ID ?? '',
 })
 
 const logger = pino(
